efe3fc2b281242a9be7809a9e4a826069c0c2099
Julio Punkito <julesnoir@hotmail.fr>
2024-12-18 12:09:10 +0100

    feat: ✨ added a barebone fuelmanager class

---
+++ scripts/FuelManager.cs
@@ -0,0 +1,25 @@
+using System;
+using Godot;
+
+[GlobalClass]
+public partial class FuelManager : Node
+{
+    [Export]
+    public float MaxFuelAmount { get; private set; } = 100;
+    public float FuelAmount { get; private set; }
+    public static event Action NoMoreFuel;
+
+    public override void _Ready()
+    {
+        FuelAmount = MaxFuelAmount;
+    }
+    public override void _Process(double delta)
+    {
+        FuelAmount -= FuelAmount > 0 ? (float)delta : 0;
+        GD.Print(FuelAmount);
+        if (FuelAmount <= 0)
+        {
+            NoMoreFuel?.Invoke();
+        }
+    }
+}
